77c9728059c45d2439721ad1f7eb5dc02121bb80
Dan Purcell <purcellgroup88@gmail.com>
2024-02-25 15:33:24 -0500

    .

--- src/utilities/wrappers/index.js
+++ src/utilities/wrappers/index.js
@@ -9,10 +9,10 @@ export const history = {
 export function fetchFromAPI(segment, options) {
   return fetch(`https://ch-th-api-6e4fa9aecbd8.herokuapp.com${segment}`, {
     credentials: "include",
-    headers: {
-      "Access-Control-Allow-Credentials": true,
-      "token": Cookies.get("token"),
-    },
+    // headers: {
+    //   "Access-Control-Allow-Credentials": true,
+    //   "token": Cookies.get("token"),
+    // },
     ...options
   }).then((res) => {
     if (res.status === 401) {
